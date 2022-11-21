c4f719b79aead6af07225c021b553957cc9a2986
Kajetan <kajetan.loska@op.pl>
2022-11-21 15:15:39 +0100

    .

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import styles from '../styles/Answer.module.css';
 export default function Answer({ answer }) {
   return (
-    <div>
-      <span>Answer</span>
-      <p>{answer}</p>
+    <div className={styles.container}>
+      <p>Answer: </p>
+      <div className={styles.inputs}>
+        {[...answer].map((char, i) => {
+          return <input key={i} value={char} className={styles.inputLetter}></input>;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Word.js
+++ src/components/Word.js
@@ -3,7 +3,7 @@ import styles from '../styles/Word.module.css';
 export default function RandomWord({ translate }) {
   const [word, setWord] = useState('');
   const [sourceLanguage, setSourceLanguage] = useState('EN');
-  const [targetLanguage, setTargetLanguage] = useState('EN');
+  const [targetLanguage, setTargetLanguage] = useState('PL');
   const languages = [
     'BG',
     'CS',
@@ -42,39 +42,48 @@ export default function RandomWord({ translate }) {
   };
 
   return (
-    <div>
-      <form>
-        <label htmlFor="source" className={styles.s}>
-          Source Language
-        </label>
-        <select
-          id="source"
-          value={sourceLanguage}
-          onChange={e => setSourceLanguage(e.target.value)}
-        >
-          {languages.map(language => {
-            return <option key={language}>{language}</option>;
-          })}
-        </select>
+    <div className={styles.container}>
+      <form className={styles.inputForm}>
+        <div>
+          <label htmlFor="source">Source Language</label>
+          <select
+            id="source"
+            value={sourceLanguage}
+            onChange={e => setSourceLanguage(e.target.value)}
+          >
+            {languages.map(language => {
+              return <option key={language}>{language}</option>;
+            })}
+          </select>
+        </div>
 
-        <input onChange={e => setWord(e.target.value)} value={word}></input>
-
-        <button type="button" onClick={() => handleClick()}>
-          Generate Word
-        </button>
-        <button type="button" onClick={() => translate(sourceLanguage, word, targetLanguage)}>
-          Translate
-        </button>
-        <label htmlFor="target">Target Language</label>
-        <select
-          id="target"
-          value={targetLanguage}
-          onChange={e => setTargetLanguage(e.target.value)}
-        >
-          {languages.map(language => {
-            return <option key={language}>{language}</option>;
-          })}
-        </select>
+        <div>
+          <input
+            onChange={e => setWord(e.target.value)}
+            value={word}
+            className={styles.input}
+          ></input>
+          <div className={styles.buttons}>
+            <button type="button" onClick={() => handleClick()}>
+              Generate Word
+            </button>
+            <button type="button" onClick={() => translate(sourceLanguage, word, targetLanguage)}>
+              Translate
+            </button>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="target">Target Language</label>
+          <select
+            id="target"
+            value={targetLanguage}
+            onChange={e => setTargetLanguage(e.target.value)}
+          >
+            {languages.map(language => {
+              return <option key={language}>{language}</option>;
+            })}
+          </select>
+        </div>
       </form>
     </div>
   );
