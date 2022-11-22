e30c767f8617540b0e47c9cdc5c25eff17fc21ea
Kajetan <kajetan.loska@op.pl>
2022-11-22 17:54:48 +0100

    .

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from '../styles/Answer.module.css';
 export default function Answer({ answer }) {
+  let inputRefs = useRef([]);
+
+  const addRef = el => {
+    if (el && !inputRefs.current.includes(el)) {
+      inputRefs.current.push(el);
+    }
+
+    console.log(inputRefs);
+  };
   return (
     <div className={styles.container}>
       <p>Answer: </p>
       <div className={styles.inputs}>
         {[...answer].map((char, i) => {
-          return <input key={i} value={char} className={styles.inputLetter}></input>;
+          return (
+            <input
+              ref={addRef}
+              maxLength={1}
+              key={i}
+              defaultValue={char}
+              className={styles.inputLetter}
+            ></input>
+          );
         })}
       </div>
     </div>
