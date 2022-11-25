f2751870315d2adcdc04b49bfabd95e9eadc0225
Kajetan <kajetan.loska@op.pl>
2022-11-25 17:47:40 +0100

    .

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -11,6 +11,7 @@ export default function Answer({ answer }) {
   return (
     <div className={styles.container}>
       <p>Answer: </p>
+
       <div className={styles.inputs}>
         {[...answer].map((char, i) => {
           return (
