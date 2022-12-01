5c48a37b88f020441e90ddc355c9947c0c828ff6
Kajetan <kajetan.loska@op.pl>
2022-12-01 17:45:26 +0100

    .

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from '../styles/Word.module.css';
 export default function RandomWord({ translate }) {
   const [word, setWord] = useState('');
+
   const [sourceLanguage, setSourceLanguage] = useState('EN');
 
   const [targetLanguage, setTargetLanguage] = useState('PL');
