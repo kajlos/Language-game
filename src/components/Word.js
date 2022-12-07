11f1bf3ae00e00defb241ef5c9799bcec2f59a22
Kajetan <kajetan.loska@op.pl>
2022-12-07 17:16:28 +0100

    .

--- src/components/Word.js
+++ src/components/Word.js
@@ -41,6 +41,7 @@ export default function RandomWord({ translate }) {
 
   const handleClick = async () => {
     let res = await fetch('https://api.api-ninjas.com/v1/randomword');
+
     let data = await res.json();
 
     setWord(data.word);
