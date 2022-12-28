7bd0af3dc8e0a4b0593dbad31726cabc34f5b867
Kajetan <kajetan.loska@op.pl>
2022-12-28 17:01:22 +0100

    .

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,27 +4,41 @@ import styles from '../styles/Answer.module.css';
 
 export default function Answer({ answer }) {
   let inputRefs = useRef([]);
-
+  console.log(answer);
+  const avaiableCharacters = /^[a-zA-Z0-9_.-]*$/;
   const addRef = el => {
     if (el && !inputRefs.current.includes(el)) {
       inputRefs.current.push(el);
     }
+    console.log(inputRefs.current);
   };
   return (
     <div className={'container'}>
       <h2 className={styles.answer}>Answer: </h2>
 
       <div className={styles.inputs}>
-        {[...answer].map((char, i) => {
-          return (
-            <input
-              ref={addRef}
-              maxLength={1}
-              key={i}
-              defaultValue={char}
-              className={styles.inputLetter}
-            ></input>
-          );
+        {[...answer].map(char => {
+          if (avaiableCharacters.test(char)) {
+            return (
+              <input
+                ref={addRef}
+                maxLength={1}
+                key={crypto.randomUUID()}
+                className={styles.inputLetter}
+              ></input>
+            );
+          } else {
+            return (
+              <input
+                ref={addRef}
+                maxLength={1}
+                key={crypto.randomUUID()}
+                defaultValue={char}
+                className={styles.inputLetter}
+                disabled
+              ></input>
+            );
+          }
         })}
       </div>
     </div>
